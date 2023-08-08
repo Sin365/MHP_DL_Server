28d934cd0d2e19269f0f37147c37067621640099
sin365 <353374337@qq.com>
2023-08-08 12:47:15 +0800

    初版归档

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$haoyeu_dbip = '127.0.0.1';
+$haoyue_dbport = 3306;
+$haoyeu_dbname = 'mhpdl';
+$haoyue_dbuser = '';
+$haoyue_dbpwd = '';
+?>
---
+++ dataconn.php
@@ -0,0 +1,4 @@
+﻿<?php
+include 'config.php';
+$con = new mysqli($haoyeu_dbip,$haoyue_dbuser,$haoyue_dbpwd,$haoyeu_dbname);
+?>
